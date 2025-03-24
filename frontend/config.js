6fbd7e669453239fe0b9843d6cf849edad528dab
bahertylop <levichankin@gmail.com>
2025-03-24 12:45:25 +0300

    переделал на nginx фронт, добавил домен

--- frontend/config.js
+++
@@ -1,3 +0,0 @@
-const CONFIG = {
-    API_BASE_URL: "https://c864-185-223-93-175.ngrok-free.app"
-};
---
+++ frontend/pages/config.js
@@ -0,0 +1,3 @@
+const CONFIG = {
+    API_BASE_URL: "https://81d5-185-223-93-175.ngrok-free.app"
+};
