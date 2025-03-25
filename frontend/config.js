50b5d49264dbf5b5cf0686570fb00099788bdb7e
bahertylop <levichankin@gmail.com>
2025-03-25 22:51:57 +0300

    неправильные ссылки на фронте

--- frontend/config.js
+++
@@ -1,3 +0,0 @@
-const CONFIG = {
-    API_BASE_URL: "https://32a1-185-223-93-175.ngrok-free.app"
-};
--- frontend/pages/config.js
+++ frontend/pages/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_BASE_URL: "https://2940-185-223-93-175.ngrok-free.app"
+    API_BASE_URL: "https://32a1-185-223-93-175.ngrok-free.app"
 };
